be215d7e81019cc4fd09e0d815522a5b4a7607c1
Tejash jamadar <tejash.jamadar@external.webasto.com>
2023-08-02 11:51:37 +0530

    new commit

--- mul.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    int a=20,b=30;
-    printf("Mul is %d",a*b);
-}
--- sum.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    int a=2,b=3;
-    printf("Sum is %d",a+b);
-}
